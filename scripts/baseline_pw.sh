ba4435fd7b05259ba7084e8df3be28bc74ea9e14
Sam <samofanci@gmail.com>
2026-08-07 17:23:42 +0200

    pw sweep added

---
+++ scripts/baseline_pw.sh
@@ -0,0 +1,34 @@
+cd /storage/brno12-cerit/home/nexuso1/phossil/model
+../scripts/pip_install.sh
+residues=$1
+splits=$2
+prot_info=$3
+type=$4
+suffix=$5
+pos_weight=$6
+
+OUT_DIRNAME=baseline_pw${pos_weight}$_"$residues"_"$type"_"$suffix"
+
+echo "Running experiment $OUT_DIRNAME with residues $residues"
+
+if compgen -G "new_logs/$OUT_DIRNAME/fold_0/*.ckpt" > /dev/null; then
+        echo "Log already exists, resuming from checkpoint"
+        regex="fold_([0-9]+)"
+        max=0
+        final_path=""
+        for path in new_logs/"$OUT_DIRNAME"/*/chkpt.ckpt;
+        do
+                dir="$(dirname $path)"
+                dir="$(basename $dir)"
+                if [[ "$dir" =~ $regex ]]
+                then
+                        num="${BASH_REMATCH[1]}"
+                        max=$(( "$num" > "$max" ? "$num" : "$max" ))
+                        final_path="$path"
+                fi
+        done
+        echo "Resuming from $final_path"
+        python3 baseline.py --checkpoint_path="$final_path"
+else
+	python3 baseline.py --epochs=30 --batch_size=4 --accum=8 --pos_weight="$pos_weight" --prot_info_path "$prot_info" --dataset_path "$splits" --lr=5e-4 -o "$OUT_DIRNAME" --num_workers=15 --residues="$residues" --modify_prob=0 --type="$type" --fold=0
+fi
