497ebe5a2c34e7c163072b542abfa38f34c8fcfc
Sam <samofanci@gmail.com>
2026-09-07 13:04:16 +0200

    removed unused functions

--- model/data_loading.py
+++ model/data_loading.py
@@ -6,15 +6,9 @@ import numpy as np
 import json
 
 from prot_dataset import FullProteinDataset, ProteinDataset
-from sklearn.model_selection import train_test_split
-from Bio import SeqIO
 from functools import partial
 from ast import literal_eval
-from constants import id_to_res, esm_valid_res_ids, esm_to_sm_id_mapping, sub_probs
-
-def remove_long_sequences(df, max_length):
-    mask = df['sequence'].apply(lambda x: len(x) < max_length)
-    return df[mask]
+from constants import  esm_valid_res_ids, esm_to_sm_id_mapping, sub_probs
 
 def labeling_fn(row, residues={'S', 'T', 'Y'}, ignore_index=-1, kinases=False):
     res = np.zeros(len(row['sequence']), dtype=np.int32) + ignore_index
@@ -234,78 +228,6 @@ def prep_batch(data, tokenizer, modify_prob=0, mask_prob=0.7, rand_prob=0.15, su
         batch['kinase_labels'] = torch.as_tensor(np.asarray([val for lst in kinase_labels for val in lst]), dtype=torch.float32)
 
     return batch
-
-def load_clusters(path):
-    return pd.read_csv(path, sep='\t', names=['cluster_rep', 'cluster_mem'])
-
-def split_train_test_clusters(args, clusters : pd.DataFrame, test_size : float):
-    reps = clusters['cluster_rep'].unique() # Unique cluster representatives
-    train, test = train_test_split(reps, test_size=test_size, random_state=args.seed)
-    return set(train), set(test)
-
-def get_train_test_prots(clusters, train_clusters, test_clusters):
-    train_mask = [x in train_clusters for x in clusters['cluster_rep']]
-    test_mask = [x in test_clusters for x in clusters['cluster_rep']]
-    train_prots = clusters['cluster_mem'][train_mask]
-    test_prots = clusters['cluster_mem'][test_mask]
-    return set(train_prots), set(test_prots)
-
-def preprocess_data(df : pd.DataFrame):
-    """
-    Preprocessing for Pbert/ProtT5. Replaces rare residues with 'X' and adds spaces between residues
-    """
-    df['sequence'] = df['sequence'].str.replace('|'.join(["O","B","U","Z"]),"X",regex=True)
-    df['sequence'] = df.apply(lambda row : " ".join(row["sequence"]), axis = 1)
-    return df
-
-def split_dataset(data : pd.DataFrame, train_clusters, test_clusters):
-    """
-    Splits data into train and test data according to train and test clusters.
-    """
-    train_mask = data['id'].apply(lambda x: x in train_clusters)
-    test_mask = data['id'].apply(lambda x: x in test_clusters)
-    return data[train_mask], data[test_mask]
-
-def load_fasta(path : str):
-    seq_iterator = SeqIO.parse(open(path), 'fasta')
-    seq_dict = {}
-    for seq in seq_iterator:
-        # extract sequence id
-        try:
-            seq_id = seq.id.split('|')[0]
-        except IndexError:
-            # For some reason, some sequences do not contain uniprot ids, so skip them
-            continue
-        seq_dict[seq_id] = str(seq.seq)
-
-    return seq_dict
-
-def load_phospho(path : str):
-    """
-    Extracts phosphoryllation site indices from the dataset. 
-    Locations expected in the column 'MOD_RSD'.
-    
-    Returns a dictionary in format {ACC_ID : [list of phosphoryllation site indices]}
-    """
-    dataset = pd.read_csv(path, sep='\t', skiprows=3)
-    dataset['position'] = dataset['MOD_RSD'].str.extract(r'[\w]([\d]+)-p')
-    grouped = dataset.groupby(dataset['ACC_ID'])
-    res = {}
-    for id, group in grouped:
-        res[id] = group['position'].to_list()
-    
-    return res
-
-def load_phospho_epsd(path : str):
-    data = pd.read_csv(path, sep='\t')
-    data.index = data['EPSD ID']
-    grouped = data.groupby(data['EPSD ID'])
-
-    res = {}
-    for id, group in grouped:
-        res[id] = group['Position'].to_list()
-
-    return res
 
 def parse_residues(residue_string):
     try:
